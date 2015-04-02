2cf389c338e9badeb2df57d54205b9555854adb0
福井優介 <fukui.yusuke@isid.co.jp>
2015-04-02 17:32:06 +0900

    monaco pull テスト

--- index.php
+++ index.php
@@ -15,4 +15,5 @@ define('WP_USE_THEMES', true);
 
 /** Loads the WordPress Environment and Template */
 /** monaco commit テスト用コメント */
+/** monaco pull テスト用コメント */
 require( dirname( __FILE__ ) . '/wp-blog-header.php' );
